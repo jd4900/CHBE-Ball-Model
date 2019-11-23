ca5bbc9a8965169f1203cf9331c0e1d2e0983a14
Joshua Donaldson <donaldson.josh.96@gmail.com>
2019-11-23 14:06:42 -0800

    init repo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ball-model',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'flake8',
+        'autopep8',
+    ],
+)
