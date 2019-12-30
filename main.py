c98fdc04e89fcbc7e03c91bab8a1efe77f3311b0
Joshua Donaldson <donaldson.josh.96@gmail.com>
2019-12-29 16:07:28 -0800

    start logging, check inputs and validate whether the inputs are correct or not

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ from kivy.properties import StringProperty
 from kivy.uix.widget import Widget
 from kivy.uix.togglebutton import ToggleButton
 from kivy.config import Config
+import logging
 
 Config.set('graphics', 'resizable', True)
+
+def validate_inputs(Hardness, Acidity, BOD):
+    if Hardness=='10 ppm' and Acidity=='7' and BOD=='Low':
+        logging.info("Dispensing good stuff")
+        return True
+    else:
+        logging.info("Dispensing bad stuff")
+        return False
 
 class BallModelUI(Widget):
 
@@ -21,13 +30,17 @@ class BallModelUI(Widget):
         elif button.group == "BOD":
             self.BOD=button.text
 
+    def check_inputs(self):
+        if any(item == None for item in [self.Hardness, self.Acidity, self.BOD]):
+            logging.warning("Somethings Not Right!")
+        else:
+            validate_inputs(self.Hardness, self.Acidity, self.BOD)
+
     def press(self, *args):
         self.get_inputs(args[0])
 
     def execute(self):
-        print(self.Hardness)
-        print(self.BOD)
-        print(self.Acidity)
+        self.check_inputs()
 
 class BallModelApp(App):
     def build(self):
