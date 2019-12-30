facbe30ad00fe4376acf5e23130c85ee4633eb0d
Joshua Donaldson <donaldson.josh.96@gmail.com>
2019-12-29 16:48:45 -0800

    get rid of bug where if button is pressed and then unpressed it still reads input - add popup warning window

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from kivy.properties import StringProperty
 from kivy.uix.widget import Widget
 from kivy.uix.togglebutton import ToggleButton
 from kivy.config import Config
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+
 import logging
 
 Config.set('graphics', 'resizable', True)
@@ -22,25 +26,52 @@ class BallModelUI(Widget):
     Acidity = None
     BOD = None
 
-    def get_inputs(self, button):
-        if button.group == "Hardness":
+    def get_inputs(self, button, state):
+        if button.group == "Hardness" and state is "down":
             self.Hardness=button.text
-        elif button.group == "Acidity":
+        elif button.group == "Acidity" and state is "down":
             self.Acidity=button.text
-        elif button.group == "BOD":
+        elif button.group == "BOD" and state is "down":
             self.BOD=button.text
+        elif button.group == "Hardness" and state is not "down":
+            self.Hardness = None
+        elif button.group == "Acidity" and state is not "down":
+            self.Acidity = None
+        elif button.group == "BOD" and state is not "down":
+            self.BOD = None
+
 
     def check_inputs(self):
         if any(item == None for item in [self.Hardness, self.Acidity, self.BOD]):
-            logging.warning("Somethings Not Right!")
+            logging.warning("Input should not be None")
+            self.mkpopup()
         else:
             validate_inputs(self.Hardness, self.Acidity, self.BOD)
 
     def press(self, *args):
-        self.get_inputs(args[0])
+        self.get_inputs(args[0], args[1])
 
     def execute(self):
         self.check_inputs()
+
+    def mkpopup(self):
+        layout = GridLayout(cols = 1, padding = 10)
+        
+        popupLabel = Label(text="Missing Input!")
+        closeButton = Button(text = "Click to Return")
+
+        layout.add_widget(popupLabel)
+        layout.add_widget(closeButton)
+
+        popup = Popup(
+            title="Warning!", 
+            content = layout
+        )
+        
+        popup.open()
+
+        closeButton.bind(on_press = popup.dismiss)
+
 
 class BallModelApp(App):
     def build(self):
