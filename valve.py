a77269c937fca2a59e3ead9c1ccc89f4350a5528
Joshua Donaldson <donaldson.josh.96@gmail.com>
2020-01-11 16:42:40 -0800

    clean up valve.py

--- valve.py
+++ valve.py
@@ -1,28 +1,33 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+GPIO_PIN_17 = 17
+GPIO_PIN_18 = 18
+
 
 def init_valve():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(18, GPIO.OUT)
-    GPIO.setup(17, GPIO.OUT)
+    GPIO.setup(GPIO_PIN_17, GPIO.OUT)
+    GPIO.setup(GPIO_PIN_18, GPIO.OUT)
+
+    close_valve()
 
 
 def open_valve():
-    GPIO.output(18, GPIO.LOW)
-    GPIO.output(17, GPIO.HIGH)
-
-    sleep(5)
+    GPIO.output(GPIO_PIN_17, GPIO.HIGH)
+    GPIO.output(GPIO_PIN_18, GPIO.LOW)
 
 
 def close_valve():
-    GPIO.output(18, GPIO.HIGH)
-    GPIO.output(17, GPIO.LOW)
-
-    sleep(5)
+    GPIO.output(GPIO_PIN_17, GPIO.LOW)
+    GPIO.output(GPIO_PIN_18, GPIO.HIGH)
 
 
 if __name__ == "__main__":
     init_valve()
+
     open_valve()
+
+    sleep(10)
+
     close_valve()
